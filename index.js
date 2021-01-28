bfa6e8aa35e5118064cee20f84f87db4c2998f92
romssilva <roms.silva@gmail.com>
2021-01-27 21:52:21 -0300

    old

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const axios = require('axios')
 const { parse } = require('node-html-parser')
 const cmd = require('node-cmd')
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const createCsvWriter = require('csv-writer').createObjectCsvWriter
+const csv = require('csvtojson')
+const path = require('path')
+const fs = require('fs')
 
 const search = require('./search')
 
 const AMOUNT_OF_PROJECTS = 100
+const AMOUNT_OF_USERS = 1000000
+
+const timeout = ms => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const auth = {
+    username: 'romssilva',
+    password: '1030psswrd'
+}
+const headers = {
+    'Accept': 'application/vnd.github.v3+json'
+}
 
 let projects = []
 
@@ -134,9 +150,123 @@ const fetchProjects = async () => {
 
 }
 
+const fetchGitHubUsers = async () => {
+    console.log('Getting ready...')
+    for (let contributionsCount = 31; contributionsCount > -1; contributionsCount--) {
+        console.log(`Requesting users with ${contributionsCount} contributions.`)
+        const filePrefix = `github_users_${contributionsCount}_${new Date().getTime()}`
+        let users = 0
+        for (let page = 1; page < 11; page++) {
+            console.log('Request #', page)
+            const url = 'https://api.github.com/search/users'
+    
+            const [githubUsersResponse] = await Promise.all([axios({
+                method: 'GET',
+                url,
+                headers,
+                auth,
+                params: {
+                    q: `repos:>10 language:javascript type:user followers:0..${contributionsCount}`,
+                    per_page: 100,
+                    page,
+                    sort: 'followers',
+                    order: 'desc'
+                }
+            }), timeout(0)]);
+            
+            const { data: { items } } = githubUsersResponse
+            console.log('Search complete!')
+            const userURLs = items.map(user => user.url)
+    
+            let responses = []
+            for (let i = 0; i < userURLs.length; i++) {
+                let currentUserUrl = userURLs[i]
+    
+                console.log(`Requesting ${i+1}th user (${currentUserUrl}) from page ${page}.`)
+                let [currentUserResponse] = await Promise.all([axios({
+                    method: 'GET',
+                    url: currentUserUrl,
+                    headers,
+                    auth
+                }), timeout(1000)]);
+    
+                responses = [...responses, currentUserResponse]
+            }
+            
+            console.log('Users request complete!')
+            const userEmails = responses.map(response => ({
+                login: response.data.login,
+                url: response.data.url,
+                html_url: response.data.html_url,
+                name: response.data.name,
+                email: response.data.email,
+                bio: response.data.bio,
+                followers: response.data.followers
+            })).filter(user => user.email)
+            users = users + userEmails.length
+            const usersWithEmail = [...userEmails]
+            console.log(`We found ${usersWithEmail.length}, users with email on this request.`)
+            console.log(`Writing to csv/github_users/${filePrefix}_${page}.csv file...`)
+            console.log(`We have ${users} users so far.`)
+            writeToCSV(`csv/github_users/${filePrefix}_${page}.csv`, [
+                {id: 'login', title: 'login'},
+                {id: 'url', title: 'url'},
+                {id: 'html_url', title: 'html_url'},
+                {id: 'name', title: 'name'},
+                {id: 'email', title: 'email'},
+                {id: 'bio', title: 'bio'},
+                {id: 'followers', title: 'followers'}
+            ], usersWithEmail)
+        }
+    }
+}
+
+const getEmails = async () => {
+    const directoryPath = path.join(__dirname, 'csv/github_users')
+    fs.readdir(directoryPath, async function (err, files) {
+        if (err) {
+            return console.log('Unable to scan directory: ' + err)
+        }
+        for (let fileIndex = 0; fileIndex < files.length; fileIndex++) {
+            const file = files[fileIndex]
+            console.log(`Checking ${file}`)
+            const csvFilePath = `${directoryPath}/${file}`
+            const users = await csv().fromFile(csvFilePath)
+            const eligibleUsers = []
+            for (let userIndex = 0; userIndex < users.length; userIndex++) {
+                const user = users[userIndex]
+                try {
+                    const [response] = await Promise.all([axios.get(user.html_url), timeout(2000)])
+                    const { data: userPage } = response;
+                    const htmlPage = parse(userPage)
+                    user.contributions = parseInt(htmlPage.querySelector('.js-yearly-contributions h2').rawText.replace(/\D/g,''))
+                    console.log(`${user.name} has ${user.contributions} contributions.`)
+                    // if (user.contributions >= 50) {
+                        eligibleUsers.push(user)
+                        // }
+                } catch (error) {
+                    console.log(`*** Error fetching ${user.html_url} ***`)
+                }
+            }
+            writeToCSV(`csv/github_users_eligible/${file}`, [
+                {id: 'login', title: 'login'},
+                {id: 'url', title: 'url'},
+                {id: 'html_url', title: 'html_url'},
+                {id: 'name', title: 'name'},
+                {id: 'email', title: 'email'},
+                {id: 'bio', title: 'bio'},
+                {id: 'followers', title: 'followers'},
+                {id: 'contributions', title: 'contributions'}
+            ], eligibleUsers)
+        }
+    })
+}
+
 const init = async () => {
     // await fetchProjects()
-    parseProjects()
+    // parseProjects()
+    // fetchGitHubUsers()
+    // getEmails()
 }
 
 console.log('Starting...')
